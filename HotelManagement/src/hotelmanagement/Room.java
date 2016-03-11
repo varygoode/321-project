fa9a36b7d8f91a271e9b15ace28f34ccbccd59e7
varygoode <varygoode@gmail.com>
2016-03-10 19:26:11 -0600

    primitive search functionality (reservations, rooms, users) added member variables and accessors/mutators for Room & User

--- HotelManagement/src/hotelmanagement/Room.java
+++ HotelManagement/src/hotelmanagement/Room.java
@@ -9,6 +9,43 @@ package hotelmanagement;
  *
  * @author timothy
  */
-public class Room {
+public class Room 
+{
+    String type;
+    int number;
+    String description;
+    double rate;
+    
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
     
 }
--- HotelManagement/src/hotelmanagement/User.java
+++ HotelManagement/src/hotelmanagement/User.java
@@ -9,6 +9,53 @@ package hotelmanagement;
  *
  * @author timothy
  */
-public class User {
+public class User 
+{
+
+    String username;
+    String password;
+    String firstName;
+    String lastName;
+    int ID;
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
     
 }
