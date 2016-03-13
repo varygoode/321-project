b64fd9342eab0c3ac2d187ae5db8f249fe810ac1
Monique <Monique@192.168.2.6>
2016-03-13 00:06:14 -0600

    Employee class to extend User

---
+++ HotelManagement/src/hotelmanagement/Employee.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelmanagement;
+
+/**
+ *
+ * @author Monique
+ */
+public class Employee extends User {
+    
+    
+    public void editReservation() 
+    {
+		
+    }
+	
+    public void editMenu() 
+    {
+		
+    }
+	
+    public void getReport() 
+    {
+		
+		
+    }
+	
+    public void cancel() 
+    {
+		
+		
+    }
+    
+}
